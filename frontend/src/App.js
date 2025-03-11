bc59a7e221f54901944ffc516bec0f60d6855e4f
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2025-03-11 21:50:28 +0530

    create user Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,33 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import SummaryApi from './common';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const dispatch = useDispatch()
+
+  const fetchUserDetails = async()=>{
+    const dataResponse = await fetch(SummaryApi.current_user.url,{
+      method : SummaryApi.current_user.method,
+      credentials : 'include'
+    })
+
+    const dataApi = await dataResponse.json()
+
+    if(dataApi.success){
+      dispatch(setUserDetails(dataApi.data))
+    }
+}
+
+  useEffect(()=>{
+    /**user Details */
+    fetchUserDetails()
+    /**user Details cart product */
+    fetchUserAddToCart()
+
+  },[])
+
   return (
     <>
     
